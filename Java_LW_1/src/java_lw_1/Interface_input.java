03a229e3efbbdb542c3f5e488c266ebd318a8964
kidaww <bulakhvad@gmail.com>
2018-05-02 20:40:41 +0300

    First deploy

---
+++ Java_LW_1/src/java_lw_1/Interface_input.java
@@ -0,0 +1,8 @@
+package java_lw_1;
+
+/**
+ * @author Olha Upyr
+ */
+public interface Interface_input {
+    public double[] readArray (char name_of_array) throws Exception;
+}
---
+++ Java_LW_1/src/java_lw_1/Interface_output.java
@@ -0,0 +1,9 @@
+package java_lw_1;
+
+/**
+ * @author Olha Upyr
+ */
+public interface Interface_output {
+    public void writeArray(double[] array);
+    public void writeException(String text);
+}
